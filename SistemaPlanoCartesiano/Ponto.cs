67b54ce16d5c0e97432215d01b3afb74dfd97ae5
Wenderson Aquino <wensantos.aquino@gmail.com>
2024-09-30 20:52:57 -0300

    Add files via upload

---
+++ SistemaPlanoCartesiano/Ponto.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPlanoCartesiano
+{
+    internal class Ponto
+    {
+        double x;
+        double y;
+
+        //CRIA UM PONTO NAS COORDENADAS (0,0):
+
+        public Ponto ()
+        {
+            this.x = 0; 
+            this.y = 0;
+        }
+
+        //RECEBE DE INÍCIO AS COORDENADAS:
+
+        public Ponto(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Ponto(Ponto outroPonto) //NESSE CASO, ELE ESTÁ PEGANDO COMO POR EXEMPLO O PONTO 2 COMO PARÂMETRO DE ENTRADA PARA O PONTO 3!
+        {
+            this.y = outroPonto.y;
+            this.x = outroPonto.x;       
+        }
+
+        //RETORNAR OS VALORES:
+
+        public double getX()
+        {
+            return this.x;
+        }
+
+        public double getY()
+        {
+            return this.y;
+        }
+
+        //MODIFICAR OS VALORES:
+
+        public void SetX(double X)
+        {
+            this.x = X;
+        }
+        public void SetY(double y)
+        {
+            this.y = y;
+        }
+
+        public void CalcularDistancia(Ponto outroPonto)
+        {
+            double x = this.x - outroPonto.x;
+            double y = this.y - outroPonto.y;
+            double distancia = Math.Sqrt(x * x + y * y);
+            Console.WriteLine(distancia);
+        }
+    }
+}
---
+++ SistemaPlanoCartesiano/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPlanoCartesiano
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ponto ponto1 = new Ponto(3,4);
+            Ponto ponto2 = new Ponto(5,6);
+            Ponto ponto3 = new Ponto(ponto2);
+          
+            ponto1.CalcularDistancia(ponto2);
+            Console.ReadKey();
+        }
+    }
+}
